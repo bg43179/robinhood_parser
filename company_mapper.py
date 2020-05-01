080bda8d1cc2b5d43e153b7c7b5c19a507c04cb0
Andrew <bg43179@gmail.com>
2020-05-01 13:30:21 -0700

    add helper methods

---
+++ company_mapper.py
@@ -0,0 +1,5 @@
+def company_mapper():
+  return [ 
+    ('NFLX', "https://robinhood.com/history/81733743-965a-4d93-b87a-6973cb9efd34"),
+    ('PINS', "https://robinhood.com/history/6e557a62-6956-4341-8f48-d39d560dc408")
+  ]
---
+++ text_helper.py
@@ -0,0 +1,35 @@
+import functools 
+
+def process(arr):
+  actions = []
+  values = []
+  for idx, val in enumerate(arr):
+    if idx % 2 == 0:
+      actions.append(val)
+    else:
+      values.append(val)
+
+  action_values = list(zip(map(convert_action, actions), map(parse_dollar, values)))
+  print(action_values)
+  result = functools.reduce(lambda acc, pairs: (acc + pairs[0] * pairs[1]), action_values, 0)
+
+  return "{:.2f}".format(result)
+
+def convert_action(action):
+  if action == 'Market Sell':
+    return 1
+  elif action == 'Dividend':
+    return 1
+  else:
+    return -1
+
+def parse_dollar(number):
+    return float(number[1:].replace(',', ''))
+
+
+if __name__ == '__main__':
+  # toss some unit test overhere
+  assert 1000.12 == parse_dollar('$1,000.12')
+  assert '2000.12' == process(['Market Sell', '$1,000.12', 'Dividend', '$2,000.99', 'Market Buy', '$1,000.99'])
+  
+  
