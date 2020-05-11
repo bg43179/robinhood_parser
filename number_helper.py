7c5ec640022d4344d3a002919239372c58be9411
eric199wen <eric199.wen@gmail.com>
2020-05-10 18:56:58 -0700

    add test for parse_dollar

--- number_helper.py
+++ number_helper.py
@@ -9,3 +9,12 @@ def parse_dollar(number):
     return float(number[start_idx:].replace(',', ''))
   except ValueError:
     return 0
+
+if __name__ == '__main__':
+  # toss some unit test overhere
+  num = '$1.2'
+  assert 1.20 == parse_dollar(num)
+
+  num = '+$1.2'
+  assert 1.20 == parse_dollar(num)
+
